5fd104ed9e478fb7c78adec7067b970e1477a841
Jonah Griffin <mrminecraft7026@gmail.com>
2018-10-18 13:24:35 -0400

    <Modify Files>

--- Mancala Group 7 Project/Mancala Group 7 Project/main.cpp
+++ Mancala Group 7 Project/Mancala Group 7 Project/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-//#include "movement.cpp"
 #include <string>
 
 void movePieces(int[], std::string[], int, int, std::string);
 
 int main() {
-	int numArr[12] = { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 };
-	std::string wordArr[12] = { "A1", "A2", "A3", "A4", "A5", "A6", "B1", "B2", "B3", "B4", "B5", "B6" };
+	int numArr[14] = { 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0 };
+	std::string wordArr[14] = { "A1", "A2", "A3", "A4", "A5", "A6", "no", "B1", "B2", "B3", "B4", "B5", "B6", "no" };
 
 	int goal1 = 0, goal2 = 0;
 	std::string test;
@@ -21,37 +20,4 @@ int main() {
 	//std::cout << "Hello World!" << std::endl;
 	system("pause");
 	return 0;
-}
-
-void movePieces(int arrNum[], std::string arrString[], int goal1, int goal2, std::string coord ) {
-	//std::string test;
-	int piecesToMove;
-	int index = 0;
-
-	//Checks what value the user entered
-	for (int i = 0; i < 12; i++) {
-		if (coord == arrString[i]) {
-			piecesToMove = arrNum[i];
-			std::cout << arrNum[1];
-			arrNum[i] = 0;
-			i = index;
-		}
-	}
-
-	for (int i = piecesToMove; i > 0; i--) {
-		index++;
-		arrNum[index] ++;
-		if (index == 6) {
-			goal1++;
-		}
-		if (index == 12) {
-			goal2++;
-			index = 0;
-		}
-	}
-
-	for (int i = 0; i < 12; i++) {
-		std::cout << arrNum[i] << " ";
-	}
-
 }
--- Mancala Group 7 Project/Mancala Group 7 Project/movement.cpp
+++ Mancala Group 7 Project/Mancala Group 7 Project/movement.cpp
@@ -2,34 +2,47 @@
 #include <string>
 
 void movePieces(int arrNum[], std::string arrString[], int goal1, int goal2, std::string coord ) {
-	std::string test;
 	int piecesToMove;
 	int index = 0;
-
 	//Checks what value the user entered
-	for (int i = 0; i < 12; i++) {
-		if (test == arrString[i]) {
+	for (int i = 0; i < 14; i++) {
+		if (coord == arrString[i]) {
 			piecesToMove = arrNum[i];
-			std::cout << arrNum[1];
 			arrNum[i] = 0;
-			i = index;
+			index = i;
 		}
 	}
 
-	for (int i = piecesToMove; i > 0; i--) {
+	for (int o = piecesToMove; o > 0; o--) {
 		index++;
 		arrNum[index] ++;
-		if (index == 6) {
-			goal1++;
+		if (index == 13) {
+			index = -1;
 		}
-		if (index == 12) {
+		/*if (index == 5) {
+			goal1++;
+			index++;
+		}
+		else if (index == 11) {
 			goal2++;
 			index = 0;
 		}
+		else {
+			index++;
+			arrNum[index] ++;
+		}*/
 	}
 
-	for (int i = 0; i < 12; i++) {
-		std::cout << arrNum[i] << " ";
+	for (int p = 0; p < 14; p++) {
+		if (p == 6) {
+			std::cout << "GOAL: " << arrNum[p] << "\n";
+		}
+		else if (p == 13) {
+			std::cout << "GOAL: " << arrNum[p] << "\n";
+		}
+		else {
+			std::cout << arrNum[p] << " ";
+		}
 	}
 
 }
