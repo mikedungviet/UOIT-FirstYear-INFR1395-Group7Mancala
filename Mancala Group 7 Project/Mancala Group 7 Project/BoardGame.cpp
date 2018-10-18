a6e8ca56185f276b976046ff584352dfde695448
Mike Pham <vietdung.pham@uoit.net>
2018-10-18 15:38:28 -0400

    <Modify File>
    Change the renderBoard function in BoardGame.cpp

--- Mancala Group 7 Project/Mancala Group 7 Project/BoardGame.cpp
+++ Mancala Group 7 Project/Mancala Group 7 Project/BoardGame.cpp
@@ -1,5 +1,5 @@
 #include "BoardGame.h"
-//#include <iomanip>
+
 
 /*
 	This function draws the board at the location set in
@@ -19,13 +19,19 @@ void BoardGame::RenderBoard(int xLocation, int yLocation) {
 	std::cout << std::setw(107+xLocation)<<" _______________________________________________________________________________________________________ \n";
 	std::cout << std::setw(107+xLocation)<<"|            |    (A1)    |    (A2)    |    (A3)    |    (A4)    |    (A5)    |    (A6)    |            |\n";
 	std::cout << std::setw(107+xLocation)<<"|            |            |            |            |            |            |            |            |\n";
-	std::cout << std::setw(20+xLocation)<<"|            |     "<<std::setw(2)<<std::setfill('0')<< testing[0].boardGameArray<<"     |     " << std::setw(2) << std::setfill('0') <<testing[1].boardGameArray<<"     |     " << std::setw(2) << std::setfill('0') <<testing[2].boardGameArray<<"     |     " << std::setw(2) << std::setfill('0') <<testing[3].boardGameArray<<"     |     " << std::setw(2) << std::setfill('0') <<testing[4].boardGameArray<<"     |     " << std::setw(2) << std::setfill('0') <<testing[5].boardGameArray<<"     |            |\n";
+	std::cout << std::setw(20+xLocation)<<"|            |     "<<std::setw(2)<<std::setfill('0')<< testing[0].boardGameValue <<"     |     " << std::setw(2) << std::setfill('0') 
+		<<testing[1].boardGameValue <<"     |     " << std::setw(2) << std::setfill('0') <<testing[2].boardGameValue <<"     |     " << std::setw(2) << std::setfill('0') <<testing[3].boardGameValue <<"     |     "
+		<< std::setw(2) << std::setfill('0') <<testing[4].boardGameValue <<"     |     " << std::setw(2) << std::setfill('0') <<testing[5].boardGameValue <<"     |            |\n";
 	std::cout << std::setfill(' ') << std::setw(107+xLocation)<<"|            |____________|____________|____________|____________|____________|____________|            |\n";
 	std::cout << std::setfill(' ')<< std::setw(107+xLocation)<<"|            |                                                                             |            |\n";
-	std::cout << std::setfill(' ')<< std::setw(7+xLocation)<<"|     " << std::setw(2) << std::setfill('0') << testing[13].boardGameArray<<"     |                               Player 1's Turn                               |     " << std::setw(2) << std::setfill('0') << testing[6].boardGameArray << "     |\n";
+	std::cout << std::setfill(' ')<< std::setw(7+xLocation)<<"|     " << std::setw(2) << std::setfill('0') << testing[13].boardGameValue <<"     |                               Player 1's Turn                               |     " 
+		<< std::setw(2) << std::setfill('0') << testing[6].boardGameValue << "     |\n";
 	std::cout << std::setfill(' ')<< std::setw(107+xLocation)<<"|            |_____________________________________________________________________________|            |\n";
 	std::cout << std::setfill(' ')<< std::setw(107+xLocation)<<"|            |            |            |            |            |            |            |            |\n";
-	std::cout << std::setfill(' ')<< std::setw(20+xLocation)<<"|            |     " << std::setw(2) << std::setfill('0') << testing[12].boardGameArray << "     |     " << std::setw(2) << std::setfill('0') << testing[11].boardGameArray << "     |     " << std::setw(2) << std::setfill('0') << testing[10].boardGameArray << "     |     " << std::setw(2) << std::setfill('0') << testing[9].boardGameArray << "     |     " << std::setw(2) << std::setfill('0') << testing[8].boardGameArray << "     |     " << std::setw(2) << std::setfill('0') << testing[7].boardGameArray << "     |            |\n";
+	std::cout << std::setfill(' ')<< std::setw(20+xLocation)<<"|            |     " << std::setw(2) << std::setfill('0') << testing[12].boardGameValue << "     |     "
+		<< std::setw(2) << std::setfill('0') << testing[11].boardGameValue << "     |     " << std::setw(2) << std::setfill('0') << testing[10].boardGameValue << "     |     " 
+		<< std::setw(2) << std::setfill('0') << testing[9].boardGameValue << "     |     "
+		<< std::setw(2) << std::setfill('0') << testing[8].boardGameValue << "     |     " << std::setw(2) << std::setfill('0') << testing[7].boardGameValue << "     |            |\n";
 	std::cout << std::setfill(' ')<< std::setw(107+xLocation)<<"|            |            |            |            |            |            |            |            |\n";
 	std::cout << std::setfill(' ')<< std::setw(107+xLocation)<<"|            |    (B1)    |    (B2)    |    (B3)    |    (B4)    |    (B5)    |    (B6)    |            |\n";
 	std::cout << std::setw(107+xLocation)<<"|____________|____________|____________|____________|____________|____________|____________|____________|\n";
