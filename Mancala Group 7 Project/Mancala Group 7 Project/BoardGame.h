77e970c808fa73bff1afea575d4030eb9acda972
Mike Pham <vietdung.pham@uoit.net>
2018-10-18 11:25:55 -0400

    <Modify Files>
    Added framework for Player.h (Variables and Constructor) and BoardGame.h (Array Structure and Constructor)

--- Mancala Group 7 Project/Mancala Group 7 Project/BoardGame.h
+++ Mancala Group 7 Project/Mancala Group 7 Project/BoardGame.h
@@ -1,27 +1,56 @@
 #pragma once
 //Header files
 #include <iostream>
+#include <string>
 #include <iomanip>
 
 
 class BoardGame
 {
 private:
-	int boardGameArray[14];
+	struct node
+	{
+		int boardGameArray;
+		std::string userChoice;
+	};
+	node *testing = new node[14];
+
 public:
 	/*
 		This is a default constructor. It will initalize
 		the board array at initial stage
 	*/
 	BoardGame() {
-		int tempArr[]{ 4,4,4,4,4,4,0,4,4,4,4,4,4,0 };
-		std::copy(tempArr, tempArr + 14, boardGameArray);
+		for (int i = 0; i < 14; i++) {
+			//Set the mancala goals
+			if (i == 6 || i == 13) {
+				testing[i].boardGameArray = 0;
+				testing[i].userChoice = "";
+			}
+			//Set the pits to their default
+			else {
+				testing[i].boardGameArray = 4;
+				if (i <= 6)
+					testing[i].userChoice = "a" + std::to_string(i++);
+				else if (i >= 7) {
+					testing[i].userChoice = "b" + std::to_string(i++);
+				}
+			}
+		}
 	}
 	/*
 		Deconstructor
 	*/
 	~BoardGame() {
 
+	}
+
+	int GetBoardGameArray(int index) {
+		return testing[index].boardGameArray;
+	}
+
+	std::string GetUserChoice(int index) {
+		return testing[index].userChoice;
 	}
 
 	//Class member function
